40f9b0d6def68bdebb1c6b9dc2cbab5f73a32d2c
ArsecTech <arshia.ghaffarian003@gmail.com>
2022-06-09 00:12:26 +0430

    UPDATE-1

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	// "encoding/json"
+	// "math/rand"
+	"net/http"
+	// "strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{Id})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies/", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Server is running on port 4000/")
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
